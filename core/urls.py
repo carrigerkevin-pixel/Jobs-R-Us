58ec367d6e02bea8b8484f7b501018655222d1a1
Kevin Carriger <carrigerkevin@gmail.com>
2026-09-21 15:07:43 -0400

    Add admin user management view for story 21

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.home, name='home'),
+    path('manage-users/', views.manage_users, name='manage_users'),
+    path('manage-users/<int:user_id>/toggle/', views.toggle_user_active, name='toggle_user_active'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-# Create your views here.
+def home(request):
+    return render(request, 'core/home.html')
+
+def is_admin(user):
+    return user.is_staff
+
+@login_required
+@user_passes_test(is_admin)
+def manage_users(request):
+    users = User.objects.all()
+    return render(request, 'core/manage_users.html', {'users': users})
+
+@login_required
+@user_passes_test(is_admin)
+def toggle_user_active(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    user.is_active = not user.is_active
+    user.save()
+    return redirect('manage_users')
