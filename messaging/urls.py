759a5473021039db23ebfe84f966ce4092bf26d3
Chevy White <chevyrwhite321@gmail.com>
2026-09-24 00:43:11 -0400

    Fixing issues caused by pushing after pull request was made.

--- messaging/urls.py
+++ messaging/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='messaging.index'),
-<<<<<<< HEAD
-    path('send/', views.send_message, name='messaging.send_message'),
-=======
->>>>>>> d607ca8bc7f5468fbbfa8dbbf9099af92dc6c52c
+    path('send/', views.send_message, name='messaging.send_message')
 ]
