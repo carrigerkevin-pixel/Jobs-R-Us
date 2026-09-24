7234735a53c94b2c51620b77182d83668d870b0a
Kevin Carriger <carrigerkevin@gmail.com>
2026-09-24 12:45:29 -0400

    Update story 21 to work with custom User model

--- core/views.py
+++ core/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required, user_passes_test
-from accounts.models import ApplicantProfile
-#fix since more than one profile type
+
+User = get_user_model()
 def home(request):
     return render(request, 'core/home.html')
 
@@ -13,8 +13,6 @@ def is_admin(user):
 @user_passes_test(is_admin)
 def manage_users(request):
     users = User.objects.all()
-    for user in users:
-        ApplicantProfile.objects.get_or_create(user=user)
     return render(request, 'core/manage_users.html', {'users': users})
 
 @login_required
@@ -29,10 +27,9 @@ def toggle_user_active(request, user_id):
 @user_passes_test(is_admin)
 def change_user_role(request, user_id):
     user = get_object_or_404(User, id=user_id)
-    profile, created = ApplicantProfile.objects.get_or_create(user=user)
     if request.method == 'POST':
         new_role = request.POST.get('role')
-        if new_role in ['seeker', 'recruiter']:
-            profile.role = new_role
-            profile.save()
+        if new_role in [User.UserType.APPLICANT, User.UserType.RECRUITER]:
+            user.userType = new_role
+            user.save()
     return redirect('manage_users')
