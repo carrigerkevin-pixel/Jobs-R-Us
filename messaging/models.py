5f9425554e9938db20b8507cbe337ae4e4a6ba35
Chevy White <chevyrwhite321@gmail.com>
2026-09-23 22:26:41 -0400

    Updated Messaging, committing so I can pull all commits from the main branch onto this branch

--- messaging/models.py
+++ messaging/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.conf import settings
+from django.contrib.auth.models import User
 # Create your models here.
+class Message(models.Model):
+    message_id = models.AutoField(primary_key=True)
+    sender = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='sent_messages'
+    )
+    recipient = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='received_messages'
+    )
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
